e74cf90375a30f85c2d96381bed880309070912f
Kiko Quesada <franciscoquesadacabello89@gmail.com>
2020-11-28 09:02:01 +0100

    feat: animations

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -2,4 +2,6 @@ const KEY_A = 65;
 const KEY_D = 68;
 const KEY_S = 83;
 const KEY_W = 87;
+const KEY_SPACE = 32;
 const SPEED = 3;
+const GROUND_LIMIT = 500;
--- assets/js/modules/soldier.js
+++ assets/js/modules/soldier.js
@@ -3,50 +3,49 @@ class Soldier {
     constructor(ctx, x, y) {
         this.ctx = ctx;
         this.x = x;
+        this.maxX = (this.ctx.canvas.width - 130);
+        this.minX = 10;
         this.vx = 0;
         this.y = y;
-        this.vy = 0;
+        this.maxY = this.ctx.canvas.height;
 
         this.sprite = new Image();
-        this.sprite.src = './img/Soldier-Sprite.png';
+        this.sprite.src = './img/soldier-sprite.png';
         this.sprite.isReady = false;
-        this.sprite.horizontalFrames = 2;
-        this.sprite.verticalFrames = 2;
+        this.sprite.horizontalFrames = 4;
+        this.sprite.verticalFrames = 4;
         this.sprite.horizontalFrameIndex = 0;
-        this.sprite.verticalFrameIndex = 1;
+        this.sprite.verticalFrameIndex = 2;
         this.sprite.onload = () => {
             this.sprite.isReady = true;
             this.sprite.frameWidth = Math.floor(this.sprite.width / this.sprite.horizontalFrames);
             this.sprite.frameHeight = Math.floor(this.sprite.height / this.sprite.verticalFrames);
             this.width = this.sprite.frameWidth;
             this.height = this.sprite.frameHeight;
-        };
+        };        
 
         this.movement = {
             right: false,
-            down: false,
             left: false,
-            up: false
         };
+
+        this.drawCount = 0;
     }
 
     onKeyEvent(event) {
         const state = event.type === 'keydown';
         switch (event.keyCode) {
             case KEY_D:
+                this.sprite.verticalFrameIndex = 2;
                 this.movement.right = state;
                 break;
-            case KEY_S:
-                this.movement.down = state;
-                break;
             case KEY_A:
+                this.sprite.verticalFrameIndex = 3;
+                this.sprite.horizontalFrameIndex = 0;
                 this.movement.left = state;
                 break;
-            case KEY_W:
-                this.movement.up = state;
-                break;
         }
-    }
+    }    
 
     draw() {
         if(this.sprite.isReady) {
@@ -61,6 +60,9 @@ class Soldier {
                 this.width,
                 this.height
             );
+
+            this.drawCount++;
+            this.animate();
         }
     }
 
@@ -69,16 +71,42 @@ class Soldier {
             this.vx = SPEED;
         } else if (this.movement.left) {
             this.vx = -SPEED;
-        } else if (this.movement.up) {
-            this.vy = -SPEED;
-        } else if (this.movement.down) {
-            this.vy = SPEED;
         } else {
             this.vx = 0;
             this.vy = 0;
         }
 
         this.x += this.vx;
-        this.y += this.vy;
+
+        if(this.x >= this.maxX) {
+            this.x = this.maxX;
+        } else if (this.x <= this.minX) {
+            this.x = this.minX;
+        }
+    }
+
+    animate() {
+        if(this.movement.right) {
+            this.animateSprite(2, 0, 4, 10);
+        } else if (this.movement.left) {
+            this.animateSprite(3, 0, 4, 10);
+        } else {
+            this.resetAnimation();
+        }
+    }
+
+    resetAnimation() {
+            this.sprite.horizontalFrameIndex = 0;
+            this.sprite.verticalFrameIndex = 2;
+    }
+
+    animateSprite(initialVerticalIndex, initialHorizontalIndex, maxHorizontalIndex, frequency) {
+        if (this.sprite.verticalFrameIndex != initialVerticalIndex) {
+            this.sprite.verticalFrameIndex = initialVerticalIndex;
+            this.sprite.horizontalFrameIndex = initialHorizontalIndex;
+        } else if (this.drawCount % frequency === 0) {
+            this.sprite.horizontalFrameIndex = (this.sprite.horizontalFrameIndex + 1) % maxHorizontalIndex;
+            this.drawCount = 0;
+        }
     }
 }
